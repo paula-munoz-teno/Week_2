f7b3914e743a22cc26579b69bb18e6773f559fca
Paula-Munoz-Teno <paulamtlaboral@gmail.com>
2024-10-22 14:32:35 +0200

    Subir R5 de RetosT7 a dia1

--- POO/RetosT7.js
+++ POO/RetosT7.js
@@ -112,11 +112,69 @@
 // persona1.Edad();
 
 
+// //R4
+// class Person
+// {
+//     constructor(nombre, apellidos,yearOfBirth, peso, altura,pelo){
+//         this.viva 
+//         this.nombre = nombre
+//         this.apellidos = apellidos
+//         this.yearOfBirth = yearOfBirth
+//         this.nacionalidad 
+//         this.peso = peso
+//         this.altura = altura
+//         this.pelo = pelo
+//         this.colorpelo 
+//         this.piel
+//         this.colorpiel
+//         this.miembrosfamilia
+//         this.amigos
+//         this.creencias
+//         this.profesion
+//         this.propiedades
+//     }
+
+//     IMC(){
+//         let resultado = this.peso/(this.altura**2);
+//         console.log(resultado);
+//         return resultado;
+//         }
+
+//     Edad(){
+//         let resultado = 2024 - this.yearOfBirth;
+//         console.log(resultado);
+//         return resultado;
+//     }
+
+//     printAll(){
+//         let resultado = this.viva + "-" + this.nombre + "-" +
+//         this.apellidos + "-" + this.yearOfBirth + "-" + this.nacionalidad
+//         + "-" + this.peso + "-" + this.altura + "-" + this.pelo + "-" +
+//         this.colorpelo + "-" + this.piel + "-" + this.colorpiel + "-" +
+//         this.miembrosfamilia + "-" +this.amigos + "-" +this.creencias + "-" +
+//         this.profesion+ "-" + this.propiedades;
+//         console.log(resultado);
+//         return resultado;
+//     }
+    
+// }
+
+// //Comprobación
+// let persona1 = new Person("Marta","Sanchez",2001, 70, 1.62);
+// console.log(persona1);
+
+// persona1.IMC();
+// persona1.Edad();
+// persona1.printAll();
+
+
+//R5
 //R4
 class Person
 {
-    constructor(nombre, apellidos,yearOfBirth, peso, altura,pelo){
-        this.viva 
+    constructor(hobbies, nombre, apellidos,yearOfBirth, peso, altura,pelo){
+        this.viva
+        this.hobbies = hobbies 
         this.nombre = nombre
         this.apellidos = apellidos
         this.yearOfBirth = yearOfBirth
@@ -156,13 +214,21 @@ class Person
         console.log(resultado);
         return resultado;
     }
+
+    printHobbies(){
+        let resultado = this.hobbies;
+        console.log(resultado);
+        return resultado; 
+
+    }
     
 }
 
 //Comprobación
-let persona1 = new Person("Marta","Sanchez",2001, 70, 1.62);
+let persona1 = new Person(["jugar", "bailar", "leer", "pintar"], "Marta","Sanchez",2001, 70, 1.62);
 console.log(persona1);
 
 persona1.IMC();
 persona1.Edad();
 persona1.printAll();
+persona1.printHobbies();
